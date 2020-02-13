021cadce7c5f7f6a046c322ef7ebcac0114d5bbe
IanCo <55019@etu.he2b.be>
2020-02-13 18:14:56 +0100

    Initial commit

---
+++ src/td2/MainMoment.java
@@ -0,0 +1,10 @@
+package td2;
+
+public class MainMoment {
+    public static void main(String[] args) {
+        Moment moment1 = new Moment(16, 55, 23);
+        Moment moment2 = new Moment(45, 35, 12);
+        Moment moment3 = new Moment(26, 05, 02);
+        System.out.println(moment1);
+    }
+}
---
+++ src/td2/Moment.java
@@ -0,0 +1,15 @@
+package td2;
+
+public class Moment {
+
+    private int seconde;
+    private int minute;
+    private int heure;
+
+    public Moment (int LesSeconde, int Lesminute, int Lheure){
+        seconde = LesSeconde;
+        minute = Lesminute;
+        heure = Lheure;
+
+    }
+}
